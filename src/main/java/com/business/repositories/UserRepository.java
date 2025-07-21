4e851b2836332c75681681bf1f51e55519c246df
Nayeem4705 <mmdnayeem4705@gmail.com>
2025-07-21 11:23:55 +0530

    first commit

---
+++ src/main/java/com/business/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.business.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.business.entities.Admin;
+import com.business.entities.User;
+
+@SuppressWarnings("unused")
+public interface UserRepository extends CrudRepository<User,Integer>
+{
+public User findUserByUemail(String email);
+}
